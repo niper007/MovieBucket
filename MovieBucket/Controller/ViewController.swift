b631b4ff35e1f2e94b9a79bc24df96289263a409
Niklas <niklaspersson1988@gmail.com>
2018-05-09 23:35:13 +0200

    CLean code, smaller buttons

--- MovieBucket/Controller/ViewController.swift
+++ MovieBucket/Controller/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController {
     //var movieTitle:UILabel!
     
     @IBOutlet weak var moviePoster: UIImageView!
-
-    
     var addedMovies = [String]()
     var movies = [Movies.Content]()
     var guideLabel:UILabel!
@@ -27,10 +25,10 @@ class ViewController: UIViewController {
     var goodGradeButton: UIButton!
     var oldMovieButton:UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        //Default value
         getData(popularButton)
         self.becomeFirstResponder() // To get shake gesture
  
@@ -38,24 +36,20 @@ class ViewController: UIViewController {
     
     func setupView(){
         self.view.backgroundColor = UIColor.black
-        guideLabel = UILabel()
         
+        guideLabel = UILabel()
         guideLabel.textAlignment = .center
         guideLabel.font = UIFont(name: guideLabel.font.fontName, size:30)
         guideLabel.textColor = .white
         guideLabel.numberOfLines = 0
         self.view.addSubview(guideLabel)
         
-        guideLabel.translatesAutoresizingMaskIntoConstraints = false
-        guideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        guideLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
-        guideLabel.text = "Shake the phone\r\nFor a movie suggestion"
-        
         popularButton = UIButton()
         popularButton.backgroundColor = .green
         popularButton.setTitleColor(.black, for: .normal)
-        popularButton.setTitle("Popular Movies",for: .normal)
+        popularButton.setTitle("Popular",for: .normal)
         popularButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        popularButton.titleLabel?.textAlignment = .center
         popularButton.tag = discoverType.popular.rawValue
         popularButton.addTarget(self, action: #selector(getData(_:)), for: .touchUpInside)
         self.view.addSubview(popularButton)
@@ -63,40 +57,48 @@ class ViewController: UIViewController {
         goodGradeButton = UIButton()
         goodGradeButton.backgroundColor = .gray
         goodGradeButton.setTitleColor(.black, for: .normal)
-        goodGradeButton.setTitle("Top rated Movies",for: .normal)
-        goodGradeButton.addTarget(self, action: #selector(getData(_:)), for: .touchUpInside)
-        goodGradeButton.tag = discoverType.rating.rawValue
+        goodGradeButton.setTitle("Good\r\nRating",for: .normal)
+        goodGradeButton.titleLabel?.numberOfLines = 0
         goodGradeButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        goodGradeButton.titleLabel?.textAlignment = .center
+        goodGradeButton.tag = discoverType.rating.rawValue
+        goodGradeButton.addTarget(self, action: #selector(getData(_:)), for: .touchUpInside)
         self.view.addSubview(goodGradeButton)
         
         oldMovieButton = UIButton()
         oldMovieButton.backgroundColor = .gray
         oldMovieButton.setTitleColor(.black, for: .normal)
-        oldMovieButton.setTitle("Movies before 2000",for: .normal)
-        oldMovieButton.addTarget(self, action: #selector(getData(_:)), for: .touchUpInside)
-        oldMovieButton.tag = discoverType.old.rawValue
+        oldMovieButton.setTitle("Before\r\n2000",for: .normal)
+        oldMovieButton.titleLabel?.numberOfLines = 0
         oldMovieButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        oldMovieButton.titleLabel?.textAlignment = .center
+        oldMovieButton.tag = discoverType.old.rawValue
+        oldMovieButton.addTarget(self, action: #selector(getData(_:)), for: .touchUpInside)
         self.view.addSubview(oldMovieButton)
         
+        //Autolayout
+        
+        guideLabel.translatesAutoresizingMaskIntoConstraints = false
+        guideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        guideLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
+        guideLabel.text = "Shake the phone\r\nFor a movie suggestion"
+        
         popularButton.translatesAutoresizingMaskIntoConstraints = false
-        popularButton.topAnchor.constraint(equalTo: guideLabel.bottomAnchor, constant: 5).isActive = true
-        popularButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        popularButton.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-     
+        popularButton.topAnchor.constraint(equalTo: guideLabel.bottomAnchor, constant: 10).isActive = true
+        popularButton.widthAnchor.constraint(equalToConstant: (screenWidth/3)-20 ).isActive = true
+        popularButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
         popularButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 64).isActive = true
         
         goodGradeButton.translatesAutoresizingMaskIntoConstraints = false
-        goodGradeButton.topAnchor.constraint(equalTo: popularButton.bottomAnchor, constant: 10).isActive = true
-        goodGradeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        goodGradeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        
+        goodGradeButton.topAnchor.constraint(equalTo: guideLabel.bottomAnchor, constant: 10).isActive = true
+        goodGradeButton.widthAnchor.constraint(equalToConstant: (screenWidth/3)-20 ).isActive = true
+        goodGradeButton.leadingAnchor.constraint(equalTo: popularButton.trailingAnchor, constant: 5).isActive = true
         goodGradeButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 64).isActive = true
         
         oldMovieButton.translatesAutoresizingMaskIntoConstraints = false
-        oldMovieButton.topAnchor.constraint(equalTo: goodGradeButton.bottomAnchor, constant: 10).isActive = true
-        oldMovieButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        oldMovieButton.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        
+        oldMovieButton.topAnchor.constraint(equalTo: guideLabel.bottomAnchor, constant: 10).isActive = true
+        oldMovieButton.leadingAnchor.constraint(equalTo: goodGradeButton.trailingAnchor, constant: 5).isActive = true
+        oldMovieButton.widthAnchor.constraint(equalToConstant: (screenWidth/3)-15 ).isActive = true
         oldMovieButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 64).isActive = true
     }
 
@@ -106,7 +108,6 @@ class ViewController: UIViewController {
         }
     }
     
-    // Enable detection of shake motion
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             if movies.count > 0 {
@@ -129,6 +130,24 @@ class ViewController: UIViewController {
     }
     
     @objc func getData(_ sender : UIButton){
+        let selectedQuery = getQueryForSelected(button:sender)
+        
+        guard let url = URL(string: selectedQuery) else {return}
+        URLSession.shared.dataTask(with:url) { (data, response, err)in
+            guard let data = data else { return }
+            
+            do{
+                let decoder = JSONDecoder()
+                let movies = try decoder.decode(Movies.self, from: data)
+                self.movies = movies.results
+                
+            } catch let jsonErr {
+                print("Error serilise stuff", jsonErr)
+            }
+            }.resume()
+    }
+    
+    func getQueryForSelected(button sender : UIButton)->String{
         var selectedQuery = ""
         switch sender.tag {
             case discoverType.popular.rawValue:
@@ -146,28 +165,11 @@ class ViewController: UIViewController {
                 sender.backgroundColor = .green
                 popularButton.backgroundColor = .gray
                 goodGradeButton.backgroundColor = .gray
-
+            
         default:
             print("nothing")
-           
         }
-        // Do any additional setup after loading the view, typically from a nib.
-        guard let url = URL(string: selectedQuery) else {return}
-        
-        URLSession.shared.dataTask(with:url) { (data, response, err)in
-            
-            guard let data = data else { return }
-            
-            do{
-                let decoder = JSONDecoder()
-                
-                let movies = try decoder.decode(Movies.self, from: data)
-                self.movies = movies.results
-                
-            } catch let jsonErr {
-                print("Error serilise stuff", jsonErr)
-            }
-            }.resume()
+        return selectedQuery
     }
     
     override func didReceiveMemoryWarning() {
@@ -177,6 +179,14 @@ class ViewController: UIViewController {
    
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    public var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    public var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
     }
     
 }
